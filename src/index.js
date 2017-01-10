f22816606fbf898297efbb2aebdf386ff6b60205
Alistair King <alistair@caida.org>
2017-01-11 10:25:44 +1300

    Finish adding bootstrap deps

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TheCommons from './TheCommons/TheCommons';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <TheCommons />,
